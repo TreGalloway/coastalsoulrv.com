8c04b321a8911d77c68315c7c0b9fe653962991f
Tre <tre@Tres-MacBook-Air.local>
2022-06-06 10:32:10 -0500

    added sections and moved assests

---
+++ src/components/sections/rentals.tsx
@@ -0,0 +1,9 @@
+import { VStack } from '@chakra-ui/react'
+import Rental1 from '../rentals/rental1'
+
+export default function Rentals() {
+    return
+    ;<VStack>
+        <Rental1 />
+    </VStack>
+}
