a3d35623c15af97a191f9237d20d4bd49c3804f3
Tre <tre@Tres-MacBook-Air.local>
2022-06-10 21:38:17 -0500

    troubleshooting build error

--- pages/contact.tsx
+++
@@ -1,7 +0,0 @@
-import Contact from '../src/components/sections/contact-section'
-import type { NextPage } from 'next'
-
-const ContactPage: NextPage = () => {
-    return <Contact />
-}
-export default ContactPage
